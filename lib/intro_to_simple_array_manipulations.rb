4098f3b2f5d4f7f8a9065334e5d56315e77ded69
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 19:00:20 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,3 +30,17 @@ end
 def using_insert(array, element)
   array.insert(3, element)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def 
